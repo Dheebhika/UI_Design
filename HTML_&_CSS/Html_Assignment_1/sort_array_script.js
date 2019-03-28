d688a21c8867c65fd9ac777606fb3be7f9e78490
Admin <admin@Intern5-MacBook-Pro.local>
2019-03-28 14:39:02 +0530

    Add the git ignore file

---
+++ HTML_&_CSS/Html_Assignment_1/sort_array_script.js
@@ -0,0 +1,9 @@
+
+
+function myFunction() {
+var fruits = ["Banana", "Orange", "Apple", "Mango"];
+document.getElementById("demo") = fruits;
+
+  fruits.sort();
+  document.getElementById("demo") = fruits;
+}
---
+++ Javascript/Script.js
@@ -0,0 +1,10 @@
+/*function change()
+{
+document.getElementById("Submit").value=document.getElementById("Submitted").value;
+}*/
+function change()
+{
+var element=document.getElementById("my_id");
+console.log(element);
+ element="submitted";
+}
